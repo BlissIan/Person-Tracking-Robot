49aea8f971a88d32712f3c39807714b5b23cf722
Ian <113797545+BlissIan@users.noreply.github.com>
2025-12-14 19:30:48 -0600

    Add WebServer process and modify Track arguments

--- Main.py
+++ Main.py
@@ -3,21 +3,27 @@ from threading import Thread
 from gimble import Gimble
 from Tracking import Track
 from Driving import Rover_control
+from View_frame import WebServer
+
 
 if __name__ == "__main__":
     q1 = Queue()
     q2 = Queue()
+    q3 = Queue(maxsize=2)
     
     # Multiprocessing for camera and gimble logic
-    Camera_process = Process(target=Track, args=(q1,))
+    Camera_process = Process(target=Track, args=(q1,q3))
     Gimble_process = Process(target=Gimble, args=(q1, q2))
     Rover_drive_thread = Thread(target=Rover_control, args=(q1, q2))
+    View_frame = Process(target =WebServer, args=(q3,))
+    
     
     try:
         # Start all processes/threads
         Camera_process.start()
         Rover_drive_thread.start()
         Gimble_process.start()
+        View_frame.start()
         
         # Wait for camera and gimble processes to finish
         Camera_process.join()
